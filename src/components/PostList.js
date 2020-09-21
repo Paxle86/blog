665197400db12a44e28170095c726f942067157f
Thanh <thanh@Thanhs-MacBook-Pro.local>
2020-09-21 22:23:59 +0700

    show list post finish

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,12 +6,27 @@ class PostList extends Component {
 	componentDidMount() {
 		this.props.fetchPosts();
 	}
+	renderList() {
+		return this.props.posts.map((post) => {
+			return (
+				<div className='item' key={post.id}>
+					<i className='lag middle aligned icon user' />
+					<div className='content'>
+						<div className='description'>
+							<h2>{post.title}</h2>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	}
 	render() {
-		return (
-			<div>
-				<h1>post list</h1>
-			</div>
-		);
+		return <div className='ui relaxed divided list'>{this.renderList()}</div>;
 	}
 }
-export default connect(null, { fetchPosts })(PostList);
+
+const mapStateToProps = (state) => {
+	return { posts: state.posts };
+};
+export default connect(mapStateToProps, { fetchPosts })(PostList);
